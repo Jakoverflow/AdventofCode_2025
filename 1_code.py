c8e91d58e4f8b04a0d092f19b0592f5eff24c103
Jakob <jakob.follmann@haw-hamburg.de>
2025-12-02 20:00:06 +0100

    expand day one, test of part 2

--- 1_code.py
+++ 1_code.py
@@ -1,22 +1,56 @@
 
+# countedzero = 0
 
-def RotateLeft(currentstate = int, clicks = int): return (currentstate - clicks)%100
+def RotateLeft(currentstate = int, clicks = int): 
+    # print(currentstate, clicks)
+    # print(currentstate - clicks)
+    # print(int((currentstate - clicks)), (currentstate - clicks) / 100)
+    # print()
+    res = currentstate - clicks
+    zeros = 0
+    print(res)
+    if (res) < 0:
+        if currentstate != 0:
+            zeros = 1
+        if -int((res)/100) >= 1:
+            zeros +=  -int((res)/100)
+            zeros += 1
+            print('links zero', zeros)
+        # elif currentstate != 0:
+        #     zeros = 1
+        # # zeros = 1 + -int((res)/100)
+        # elif currentstate > (res)%100:
+        #     zeros += 1
+        # print((res) / 100)
+        # print(zeros)
+    elif res == 0:
+        zeros = 1
 
-def RotateRight(currentstate = int, clicks = int): return (currentstate + clicks)%100
+    return (res)%100, zeros
+
+def RotateRight(currentstate = int, clicks = int): 
+
+    return (currentstate + clicks)%100, int((currentstate + clicks) / 100)
 
 def main():
     countedzero = 0
     currentstate = 50
 
     for move in open("./1_input.txt"):
+        print('-------- new ---------', move.strip())
+        print("Current", currentstate)
+        print("Zeros", countedzero)
 
         if move.startswith('L'):
-            currentstate = RotateLeft(currentstate, int(move[1:].strip()))
+            currentstate, zeros = RotateLeft(currentstate, int(move[1:].strip()))
         elif move.startswith('R'):
-            currentstate = RotateRight(currentstate, int(move[1:].strip()))
+            currentstate, zeros = RotateRight(currentstate, int(move[1:].strip()))
+            print('rechts',zeros)
+        countedzero += zeros
+        # if currentstate == 0:
+        #     countedzero += 1
+        print("Zeros", countedzero, "cur", currentstate)
 
-        if currentstate == 0:
-            countedzero += 1
 
     print(countedzero)
 
